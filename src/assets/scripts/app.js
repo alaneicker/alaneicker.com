d29fb80d92f8c4eb6842f740defaf98106bb7a90
Alan Eicker <alaneicker@gmail.com>
2019-04-30 21:44:40 -0500

    Adds logic to select menu item on scroll

--- src/assets/scripts/app.js
+++ src/assets/scripts/app.js
@@ -7,3 +7,51 @@ Array.prototype.forEach.call(document.querySelectorAll('.local-link'), function 
     e.target.parentElement.classList.add('is-active');
   });
 });
+
+document.querySelector('#mobile-nav-btn').addEventListener('click', function () {
+  const mobileNav = document.querySelector('#main-nav');
+  const isOpen = mobileNav.classList.contains('is-open');
+  mobileNav.classList[isOpen ? 'remove' : 'add']('is-open');
+});
+
+document.querySelector('#layout-body').addEventListener('scroll', function (e) {
+
+  const getTopOffset = function (id) {
+    return document.querySelector(id).offsetTop;
+  };
+
+  const setSelected = function (hash) {
+    const element = document.querySelector('#main-nav > li > [href="' + hash + '"]');
+    const siblingLinks = element.parentElement.parentElement.children;
+    
+    [...siblingLinks].forEach(function (link) { link.classList.remove('is-active') });
+    
+    element.parentElement.classList.add('is-active'); 
+  };
+
+  const scrollY = e.target.scrollTop;
+  const skillsTopOffset = getTopOffset('#skills');
+  const codeExperiementsTopOffset = getTopOffset('#code-experiments');
+  const careerJourneyTopOffset = getTopOffset('#career-journey');
+  const kudosTopOffset = getTopOffset('#kudos');
+
+  let selectedNavItem = '#hero';
+
+  if (scrollY >= skillsTopOffset) {
+    selectedNavItem = '#skills';
+  } 
+  
+  if (scrollY >= codeExperiementsTopOffset) {
+    selectedNavItem = '#code-experiments';
+  } 
+
+  if (scrollY >= careerJourneyTopOffset) {
+    selectedNavItem = '#career-journey';
+  } 
+
+  if (scrollY >= kudosTopOffset) {
+    selectedNavItem = '#kudos';
+  } 
+  
+  setSelected(selectedNavItem);
+});
